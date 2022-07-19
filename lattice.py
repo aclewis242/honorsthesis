36c5a971ae780d6e4950eea95395ec112b9851ce
aclewis242 <aclewis242@gmail.com>
2022-07-19 15:01:32 -0700

    Pre-demon implementation

--- lattice.py
+++ lattice.py
@@ -8,21 +8,22 @@ class Lattice:
     lat = np.empty(0, dtype=st.Site)
     J = 1
     B = 0
+    E = 0
+    size = 0
 
-    def __init__(self, size: int=500, intEn: float=1, magF: float=0, dir: bool=None):
-        self.J = intEn
-        self.B = magF
+    def __init__(self, size: int=20, intEn: float=1, magF: float=0, dir: bool=None):
+        self.J = intEn # Interaction energy (keep this at 1)
+        self.B = magF # Magnetic field
         self.lat = np.empty(size, dtype=st.Site)
-        for s in range(self.size()):
+        self.size = size
+        for s in range(self.size):
             spin = True
             if dir is not None: spin = dir
             else: spin = randState()
             self.lat[s] = st.Site(ud=spin)
-        for s in range(self.size()):
-            self.lat[s].setNs(self.lat[s-1], self.lat[(s+1)%self.size()])
-    
-    def size(self):
-        return self.lat.size
+        for s in range(self.size):
+            self.lat[s].setNs(self.lat[s-1], self.lat[(s+1)%self.size])
+        self.E = self.energy()
     
     def energy(self):
         tot = 0.0
@@ -30,21 +31,24 @@ class Lattice:
             tot += -self.J*s*s.rn - self.B*s
         return tot
     
-    def metropolis(self, temp: float=200):
+    def spin(self):
+        return np.mean(self.lat)
+    
+    def metropolis(self, energy, temp: float=1):
         b = 1/temp # Measured relative to the Boltzmann constant, i.e. k = 1
-        siteInd = np.random.choice(range(self.size()))
+        siteInd = np.random.choice(range(self.size))
         site = self.lat[siteInd]
-        currE = self.energy()
-        newE = currE + 2*(self.J*site*(site.rn + site.ln) + self.B*site)
+        newE = energy + 2*(self.J*site*(site.rn + site.ln) + self.B*site)
         doFlip = 1
-        if newE > currE:
-            probOfFlip = np.e**(-b*(newE - currE))
+        if newE > energy:
+            probOfFlip = np.e**(-b*(newE - energy))
             doFlip = np.random.choice([0, 1], p=[1-probOfFlip, probOfFlip])
         if doFlip:
             self.lat[siteInd].flip()
-        return self.lat
+            energy = newE
+        return self.lat, energy
     
     def sweep(self):
-        for i in range(self.size()):
-            self.lat = self.metropolis()
-        return self.energy()/self.size()
+        for i in range(self.size):
+            self.lat, self.E = self.metropolis(self.E)
+        return self.E/self.size
--- main.py
+++ main.py
@@ -4,18 +4,28 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     l = lat.Lattice()
-    numSteps = 500
+    numSteps = 500 # "Time"
     steps = range(numSteps)
     energies = np.zeros_like(steps, dtype=float)
     energies = np.append(energies, 0.0)
-    energies[0] = l.energy()/l.size()
-    print(energies[0])
+    spins = np.zeros_like(energies, dtype=float)
+    energies[0] = l.energy()/l.size
+    spins[0] = l.spin()
     for i in steps:
         energies[i+1] = l.sweep()
+        spins[i+1] = l.spin()
     steps = range(numSteps+1)
     
+    # ENERGIES
     plt.plot(steps, energies)
     plt.ylabel("Energy")
     plt.xlabel("Timestep")
     plt.savefig("energy.png")
+    plt.show()
+
+    # SPINS
+    plt.plot(steps, spins)
+    plt.ylabel("Spin direction")
+    plt.xlabel("Timestep")
+    plt.savefig("spin.png")
     plt.show()
