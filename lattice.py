f10c5b13a453d902424763fb0e3f350ab412a2d4
aclewis242 <aclewis242@gmail.com>
2022-10-25 23:11:11 -0700

    actually implemented demons properly this time (I think); reversibility is observed (!)

--- lattice.py
+++ lattice.py
@@ -1,9 +1,6 @@
 import numpy as np
 import sites as st
 
-def randState():
-    return np.random.choice(np.array([True, False]))
-
 class Lattice:
     lat = np.empty(0, dtype=st.Site)
     J = 1
@@ -11,16 +8,22 @@ class Lattice:
     E = 0
     dE = 0
     size = 0
+    # order = np.empty(0)
 
-    def __init__(self, size: int=20, intEn: float=1, demonEn: float=5, magF: float=0, dir: bool=False): # Initialises lattice
+    def __init__(self, size: int=100, intEn: float=1, demonEn: float=1, magF: float=0, dir: bool=None): # Initialises lattice
         self.J = intEn # Interaction energy (keep this at 1)
         self.B = magF # Magnetic field
-        self.lat = np.empty(size, dtype=st.Site)
         self.size = size
+        self.lat = np.empty(size, dtype=st.Site)
+        # self.order = np.array([14, 6, 9, 0, 8, 19, 2, 16, 12, 11, 3, 18, 10, 15, 7, 4, 1, 5, 17, 13])
+        # self.order = np.linspace(0, size-1, size)
+        # rand.shuffle(self.order)
+        altSpin = True
         for s in range(self.size):
             spin = True
             if dir is not None: spin = dir
-            else: spin = randState()
+            else: spin = altSpin
+            altSpin = not altSpin
             self.lat[s] = st.Site(ud=spin)
         for s in range(self.size):
             self.lat[s].setNs(self.lat[s-1], self.lat[(s+1)%self.size])
@@ -36,10 +39,9 @@ class Lattice:
     def spin(self): # Returns overall spin direction
         return np.mean(self.lat)
     
-    def metropolis(self, temp: float=1): # Implementation of the Metropolis algorithm
+    def metropolis(self, ind: int=1, temp: float=1): # Implementation of the Metropolis algorithm
         b = 1/temp # Measured relative to the Boltzmann constant, i.e. k = 1
-        siteInd = np.random.choice(range(self.size)) # Selects random site
-        site = self.lat[siteInd]
+        site = self.lat[ind]
         diffE = 2*(self.J*site*(site.rn + site.ln) + self.B*site) # Calculates difference in pre- and post-flip energy
         # To find probability of the flip occurring:
         doFlip = 1
@@ -47,22 +49,31 @@ class Lattice:
             probOfFlip = np.e**(-b*(diffE))
             doFlip = np.random.choice([0, 1], p=[1-probOfFlip, probOfFlip])
         if doFlip:
-            self.lat[siteInd].flip()
+            self.lat[ind].flip()
             self.E += diffE
         return self.lat
 
-    def metroDemon(self): # Implementation of the Metropolis algorithm, demon ver. (Creutz)
-        siteInd = np.random.choice(range(self.size)) # Selects random site
-        site = self.lat[siteInd]
+    def metroDemon(self, ind: int=1): # Implementation of the Metropolis algorithm, demon ver. (Creutz)
+        site = self.lat[ind]
         diffE = 2*(self.J*site*(site.rn + site.ln) + self.B*site) # Calculates difference in pre- and post-flip energy
         newDE = self.dE - diffE
-        if newDE > 0.5:
-            self.lat[siteInd].flip()
+        if newDE > 1: # and (site*site.rn<0 or site*site.ln<0):
+            self.lat[ind].flip()
             self.dE = newDE
             self.E += diffE
         return self.lat
     
-    def sweep(self, alg=metropolis):
-        for i in range(self.size):
-            self.lat = alg()
-        return self.E/self.size
+    # def sweep(self, order, alg=metroDemon):
+    #     for i in order:
+    #         self.lat = alg(ind=int(i))
+    #     return self.E/self.size
+    
+    # def printIt(self, alg=metroDemon):
+    #     steps = range(self.size)
+    #     Es = np.zeros_like(steps)
+    #     for i in self.order:
+    #         self.lat = alg(ind=int(i))
+    #         Es[np.where(self.order==i)] = self.E
+    #     plt.plot(steps, Es)
+    #     plt.savefig("it.png")
+    #     plt.close()
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import numpy as np
 import lattice as lat
 import matplotlib.pyplot as plt
+import random as rand
 
 if __name__ == "__main__":
-    l = lat.Lattice()
+    ### SPECIFY INPUT PARAMETERS HERE ###
+    size = 100
+    useDemon = True
     numSteps = 500 # "Time"
+
+    l = lat.Lattice(size=size)
     steps = range(numSteps)
     energies = np.zeros_like(steps, dtype=float)
     energies = np.append(energies, 0.0)
@@ -13,22 +18,28 @@ if __name__ == "__main__":
     energies[0] = l.energy()/l.size
     demonEnergies[0] = l.dE/l.size
     spins[0] = l.spin()
-    useDemon = False
     name = "-ising"
     algr = l.metropolis
     if useDemon:
         name = "-demon"
         algr = l.metroDemon
+    order = np.zeros_like(steps)
     for i in steps:
-        energies[i+1] = l.sweep(alg=algr)
+        if i < numSteps/2:
+            order[i] = int(rand.random()*100)
+        else:
+            order[i] = order[steps[-1]-i]
+    for i in range(order.size):
+        l.lat = algr(ind=order[i])
+        energies[i+1] = l.E/l.size
         demonEnergies[i+1] = l.dE/l.size
         spins[i+1] = l.spin()
     steps = range(numSteps+1)
     
     # ENERGIES
-    plt.plot(steps, energies, label='System energies')
+    plt.plot(steps, energies, label='System energy')
     if demonEnergies[0] != demonEnergies[-1] or demonEnergies[0] != demonEnergies[1]:
-        plt.plot(steps, demonEnergies, label='Demon energies')
+        plt.plot(steps, demonEnergies, label='Demon energy')
         plt.plot(steps, demonEnergies + energies, label='Total energy')
         plt.legend()
     plt.ylabel("Energy")
